53e9a970fce6dcbe01d80452e39b0f2a7bfd80d7
ScarletMaiden <natalia.nowicka1405@op.pl>
2026-03-20 12:28:06 +0100

    Update simple_map.py

--- simple_map.py
+++ simple_map.py
@@ -81,7 +81,7 @@ def render_simple_map(df: pd.DataFrame):
         lon="longitude",
         size="size",
         color="cases",
-        color_continuous_scale=["#FFFFE0", "#8B0000", "#000000"],
+        color_continuous_scale=["#C79E33", "#8B0000", "#000000"],
         hover_name="Powiat",
         hover_data={"cases": True},
         zoom=5,
